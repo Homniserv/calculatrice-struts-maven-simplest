9f73ddee7a7eb8b6e9b1d5be8911354cc75184ca
Peter MOUEZA <mouezapeter@gmail.com>
2018-03-13 10:10:17 +0100

    3 tiers + forms
    TODO Struts calendar picker

---
+++ calculatriceStrutsMavenSIMPLEST/src/main/java/presentation/MatchAction.java
@@ -0,0 +1,71 @@
+package presentation;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+//import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author info@tutorials4u.net
+ */
+
+public class MatchAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer a;
+	private Integer b;
+	private Integer res;
+
+	public MatchAction() {
+	}
+
+	public String plus() {
+		res = a + b;
+		return "SUCCESS";
+	}
+
+	public String moins() {
+		res = a - b;
+		return "SUCCESS";
+	}
+
+	public String fois() {
+		res = a * b;
+		return "SUCCESS";
+	}
+
+	public String divi() {
+		res = a / b;
+		return "SUCCESS";
+	}
+
+	public Integer getA() {
+		return a;
+	}
+
+	public void setA(Integer a) {
+		this.a = a;
+	}
+
+	public Integer getB() {
+		return b;
+	}
+
+	public void setB(Integer b) {
+		this.b = b;
+	}
+
+	public Integer getRes() {
+		return res;
+	}
+
+	public void setRes(Integer res) {
+		this.res = res;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
---
+++ calculatriceStrutsMavenSIMPLEST/src/main/webapp/WEB-INF/user.jsp
@@ -0,0 +1,23 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<%@taglib uri="/struts-tags" prefix="s" %>
+<html>
+
+<head>
+	<title>USER</title>
+</head>
+<body>
+
+	<div id="title" align="center">
+		<h1>User</h1>
+	</div>
+	<hr />
+	<hr />
+
+	<form action=""></form>
+
+</body>
+
+
+</html>
